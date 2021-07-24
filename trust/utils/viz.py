e207301e9e74d806dd773bea0375cd141cf13050
Suraj Kothawade <suraj12378@gmail.com>
2021-07-24 00:57:58 -0500

    [TSNE] Add TSNE for SMI

---
+++ trust/utils/viz.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.manifold import TSNE
+import time
+from matplotlib import pyplot as plt
+
+def tsne_smi(lake_set, lake_targets, query_set, query_targets, selected_idx, savePath=""):
+    colors = ['blue', 'purple', 'turquoise', 'green', 'red', 'lime', 'cyan', 'orange', 'gray']
+    lake_tsne = TSNE(n_components=2).fit_transform(lake_set)
+    query_tsne = TSNE(n_components=2).fit_transform(query_set)
+    lake_idx = list(set(list(range(len(lake_tsne)))) - set(selected_idx))
+    for i in lake_idx:
+        plt.scatter(lake_tsne[i][0],lake_tsne[i][1], c=colors[lake_targets[i]])
+    for i in range(len(query_tsne)):
+        plt.scatter(query_tsne[i][0],query_tsne[i][1], c=colors[query_targets[i]],edgecolors='black')
+    for i in selected_idx:
+        plt.scatter(lake_tsne[i][0],lake_tsne[i][1], c=colors[lake_targets[i]], edgecolors='yellow')
+    return plt
