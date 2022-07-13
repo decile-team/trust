f2fd905c324af2cb620a11d94f8c358ab1340acb
badrinath-newzera <badrinath@newzra.com>
2022-07-13 18:13:44 +0530

    added model for mnist

--- trust/utils/models/lenet_mnist.py
+++ trust/utils/models/lenet_mnist.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 class LeNetMNIST(nn.Module):
     def __init__(self, num_classes=10):
-        super(LeNet, self).__init__()
+        super(LeNetMNIST, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16*5*5, 120)
