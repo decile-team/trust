5d4e91d23158335cfa0d622ba6e7cd5ef7f36db0
badrinath-newzera <badrinath@newzra.com>
2022-07-13 19:03:46 +0530

    added model for mnist

--- trust/utils/custom_dataset.py
+++ trust/utils/custom_dataset.py
@@ -548,7 +548,7 @@ def load_dataset_custom(datadir, dset_name, feature, split_cfg, augVal=False, da
                 train_set, val_set, lake_set, imb_cls_idx = create_class_imb(dset_name, fullset, split_cfg, num_cls, augVal)
             print("MNIST Custom dataset stats: Train size: ", len(train_set), "Val size: ", len(val_set), "Lake size: ", len(lake_set))
             test_set = SubsetWithTargetsSingleChannel(
-                test_set, list([i for i in range(len(test_set))]), torch.Tensor(test_set.targets.float())[train_idx])
+                test_set, list([i for i in range(len(test_set))]), torch.Tensor(test_set.targets.float())[list([i for i in range(len(test_set))])])
             return train_set, val_set, test_set, lake_set, imb_cls_idx, num_cls
         if(feature=="ood"):
             train_set, val_set, test_set, lake_set, ood_cls_idx = create_ood_data(dset_name, fullset, test_set, split_cfg, num_cls, augVal)
