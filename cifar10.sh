a3ac80a522b201e6a581199c0624743c23a10653
venkatapathy <venkat.s.iyer@gmail.com>
2023-10-31 10:11:00 +0530

    bash friendly

--- cifar10.sh
+++ cifar10.sh
@@ -1,2 +1,8 @@
 python3 tutorials/All_Wassal/wassal_cifar10_multiclass_vanilla.py 2>&1 | tee tutorials/results/cifar10.log
+#!/bin/bash
+
+# Define the skip_strategies and skip_budgets
+SKIP_STRATEGIES="strategy1 strategy2"
+SKIP_BUDGETS="100 200"
+
 python3 informme.py
