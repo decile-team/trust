7aa2b17364cb4feea2b2ce6ca1b696e2e23efadb
venkatapathy <venkat.s.iyer@gmail.com>
2023-11-05 13:44:53 +0530

    al small

--- cifar10ALonly.sh
+++ cifar10ALonly.sh
@@ -5,5 +5,5 @@ SKIP_STRATEGIES="AL_WITHSOFT WASSAL WASSAL_WITHSOFT"
 SKIP_BUDGETS="25 50 75 100 200"
 DEVICE_ID="2"
 # Call the Python script with the defined arguments
-python3 tutorials/All_Wassal/wassal_cifar10_multiclass_vanilla.py "$SKIP_STRATEGIES" "$SKIP_BUDGETS" "$DEVICE_ID" 2>&1 | tee tutorials/results/cifar10al.log
+python3 tutorials/All_Wassal/wassal_cifar10_multiclass_vanilla.py "$SKIP_STRATEGIES" "$SKIP_BUDGETS" "$DEVICE_ID" 2>&1 | tee tutorials/results/cifar10al_small.log
 python3 informme.py
